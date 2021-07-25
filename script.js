806b6266eea68179a151a5ef687bbeaf50a83f2a
kiranpratyush <pratyushkiranrout@gmail.com>
2021-07-25 13:35:57 +0530

    done birthday project
    birthday mark 11

---
+++ script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const dateOfBirth = document.querySelector("#birthday");
+const luckyNumber = document.querySelector("#lucky");
+const btn = document.querySelector("button");
+const resultElement = document.querySelector(".result");
+const resultText = document.querySelector(".resultText");
+
+function checkLucky(number1, number2) {
+  let result = 0;
+  for (let i = 0; i < number1.length; i++) {
+    if (number1[i] == "-") {
+      continue;
+    } else {
+      result += Number(number1[i]);
+    }
+  }
+  console.log(result)
+  if (result % number2 === 0) {
+    console.log("True");
+    resultElement.classList.toggle("display");
+    resultElement.classList.add("success");
+    resultText.textContent = "Yay! You are lucky";
+  } else {
+    resultElement.classList.toggle("display");
+    resultElement.classList.add("faliure");
+    resultText.textContent = "Sorry! This number is Not Lucky";
+  }
+}
+
+btn.addEventListener("click", function () {
+  checkLucky(dateOfBirth.value, Number(luckyNumber.value));
+});
+resultElement.addEventListener("click", function () {
+  resultElement.classList.toggle("display");
+  if (resultElement.classList.contains("success")) {
+    resultElement.classList.remove("success");
+  } else {
+    resultElement.classList.remove("faliure");
+  }
+});
